84291c0518368ab51047fa6bec737a55c3b4dd2f
Melanie Rodas <mrodasl@uni.pe>
2022-09-29 15:42:10 -0500

    added a header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./componentes/Header";
 
 function App() {
   return (
     <>
+      <Header></Header>
       <h2>Hello Wold 🙂 </h2>
     </>
   );
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Header() {
+  return (
+    <>
+      <figure className="jobs-header"></figure>
+    </>
+  );
+}
