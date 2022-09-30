3afba8844da32083ef5e3648bfc8875f7676fd30
Melanie Rodas <mrodasl@uni.pe>
2022-09-30 11:41:37 -0500

    added a component called JobCard without props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./componentes/Header";
+import JobCard from "./componentes/JobCard";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <h2>Hello Wold 🙂 </h2>
+      <JobCard></JobCard>
     </>
   );
 }
---
+++ src/componentes/JobCard/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+export default function JobCard() {
+  return (
+    <>
+      <div className="job-card">
+        <div className="job-logo">
+          <figure>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"
+              alt="company logo"
+            />
+          </figure>
+        </div>
+        <div className="job-description">
+          <h3 className="job-company-name">Photosnap</h3>
+          <h2 className="job-position">Senior Frontend Developer</h2>
+          <p className="job-details">1d ago • Full Time • USA only </p>
+        </div>
+        <div className="job-tags">
+          <span>Frontend</span>
+          <span>Senior</span>
+          <span>HTML</span>
+          <span>CSS</span>
+          <span>JavaScript</span>
+        </div>
+      </div>
+    </>
+  );
+}
